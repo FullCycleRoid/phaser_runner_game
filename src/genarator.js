4331316c47af6b37de5ddcc70fbc3b25f18b6404
Kirill <k.vasiliev@drweb.com>
2025-05-23 13:24:15 +0300

    init runner

---
+++ src/genarator.js
@@ -0,0 +1,52 @@
+export default class Generator {
+    constructor(scene) {
+        this.scene = scene;
+        this.scene.time.delayedCall(2000, () => this.init(), null, this);
+        this.pinos = 0;
+    }
+    init() {
+        this.generateCloud();
+        this.generateObstacle();
+        this.generateCoin();
+    }
+}
+
+generateCloud() {
+    new Cloud(this.scene);
+    this.scene.time.delayedCall(
+    Phaser.Math.Between(2000, 3000),
+        () => this.generateCloud(),
+    null,
+    this
+    );
+}
+generateObstacle() {
+    this.scene.obstacles.add(
+        new Obstacle(
+            this.scene,
+            800,
+            this.scene.height - Phaser.Math.Between(32, 128)
+        )
+    );
+    this.scene.time.delayedCall(
+        Phaser.Math.Between(1500, 2500),
+        () => this.generateObstacle(),
+        null,
+        this
+    );
+}
+generateCoin() {
+this.scene.coins.add(
+new Coin(
+this.scene,
+800,
+this.scene.height - Phaser.Math.Between(32, 128)
+)
+);
+this.scene.time.delayedCall(
+Phaser.Math.Between(500, 1500),
+() => this.generateCoin(1),
+null,
+this
+);
+}
